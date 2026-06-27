00477b3cf5f50830e0bdfe6ccad7b3b60e9ec6bd
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:28:13 -0700

    Add Logger class for logging functionality
    Implemented a Logger class for logging messages to console and files. Added methods for setting output files and logging in both text and CSV formats.

--- src/utils/Logger.cpp
+++ src/utils/Logger.cpp
@@ -1,1 +1,43 @@
+#include "utils/Logger.hpp"
+#include <chrono>
+#include <iomanip>
 
+namespace Microgrid::Utils {
+
+Logger& Logger::getInstance() {
+    static Logger instance;
+    return instance;
+}
+
+void Logger::setOutputFile(const std::string& filename) {
+    fileStream.open(filename + ".log", std::ios::out | std::ios::app);
+    csvStream.open(filename + ".csv", std::ios::out);
+    csvStream << "Time,Irradiance,Temp,PV_V,PV_I,PV_P,MPPT_Duty,Bat_P,Bat_SOC,Load_P,Net_Grid\n";
+}
+
+void Logger::log(Level level, const std::string& component, const std::string& message) {
+    std::string lvlStr;
+    switch(level) {
+        case Level::INFO: lvlStr = "[INFO]"; break;
+        case Level::WARNING: lvlStr = "[WARN]"; break;
+        case Level::ERROR: lvlStr = "[ERR ]"; break;
+        case Level::DEBUG: lvlStr = "[DBUG]"; break;
+    }
+    std::cout << lvlStr << " [" << component << "] " << message << "\n";
+    if (fileStream.is_open()) {
+        fileStream << lvlStr << " [" << component << "] " << message << "\n";
+    }
+}
+
+void Logger::logCsv(const std::string& csvData) {
+    if (csvStream.is_open()) {
+        csvStream << csvData << "\n";
+    }
+}
+
+Logger::~Logger() {
+    if (fileStream.is_open()) fileStream.close();
+    if (csvStream.is_open()) csvStream.close();
+}
+
+}
