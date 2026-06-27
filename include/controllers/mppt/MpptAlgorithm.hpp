a1ee04841fdbb7edfe6d1183a65b3268b35a47eb
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:30:18 -0700

    Add MpptAlgorithm class for duty cycle calculation

--- include/controllers/mppt/MpptAlgorithm.hpp
+++ include/controllers/mppt/MpptAlgorithm.hpp
@@ -1,1 +1,15 @@
+#pragma once
 
+namespace Microgrid::Controllers::MPPT {
+    class MpptAlgorithm {
+    public:
+        virtual ~MpptAlgorithm() = default;
+        
+        // Feed current V and I, returns new calculated Duty Cycle
+        virtual double calculateDutyCycle(double voltage, double current) = 0;
+        
+    protected:
+        double currentDuty = 0.5;
+        double stepSize = 0.01;
+    };
+}
