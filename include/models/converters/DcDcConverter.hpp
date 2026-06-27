550cf7c405b6f1c408bc95f667867a9a202a50a6
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:28:55 -0700

    Add DcDcConverter class with efficiency calculations

--- include/models/converters/DcDcConverter.hpp
+++ include/models/converters/DcDcConverter.hpp
@@ -1,1 +1,18 @@
+#pragma once
 
+namespace Microgrid::Models {
+    class DcDcConverter {
+    public:
+        enum class Topology { BUCK, BOOST, BUCK_BOOST };
+
+        DcDcConverter(Topology type, double efficiencyBase);
+
+        // Applies duty cycle D (0 to 1), maps input V to output V
+        [[nodiscard]] double calculateInputVoltage(double vOutBus, double dutyCycle) const;
+        [[nodiscard]] double calculateEfficiency(double inputPower) const;
+
+    private:
+        Topology topology;
+        double baseEff;
+    };
+}
