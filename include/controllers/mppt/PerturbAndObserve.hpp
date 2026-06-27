9c768ff3d4ff2ebda7d314ee24a55321cb2e2084
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:30:28 -0700

    Update PerturbAndObserve.hpp

--- include/controllers/mppt/PerturbAndObserve.hpp
+++ include/controllers/mppt/PerturbAndObserve.hpp
@@ -1,1 +1,14 @@
+#pragma once
+#include "MpptAlgorithm.hpp"
 
+namespace Microgrid::Controllers::MPPT {
+    class PerturbAndObserve : public MpptAlgorithm {
+    public:
+        PerturbAndObserve(double initialDuty, double step);
+        double calculateDutyCycle(double voltage, double current) override;
+
+    private:
+        double prevVoltage = 0.0;
+        double prevPower = 0.0;
+    };
+}
