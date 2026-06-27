453432919a06763af943c1739c665541cb9bb3d4
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:31:13 -0700

    Add IncrementalConductance class for MPPT control

--- include/controllers/mppt/IncrementalConductance.hpp
+++ include/controllers/mppt/IncrementalConductance.hpp
@@ -1,1 +1,14 @@
+#pragma once
+#include "MpptAlgorithm.hpp"
 
+namespace Microgrid::Controllers::MPPT {
+    class IncrementalConductance : public MpptAlgorithm {
+    public:
+        IncrementalConductance(double initialDuty, double step);
+        double calculateDutyCycle(double voltage, double current) override;
+
+    private:
+        double prevVoltage = 0.0;
+        double prevCurrent = 0.0;
+    };
+}
