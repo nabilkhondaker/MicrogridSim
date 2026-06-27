bdf587de68dd2380f7f70e6bc98a7e6711808177
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:31:44 -0700

    Add DispatchController class for microgrid management

--- include/controllers/microgrid/DispatchController.hpp
+++ include/controllers/microgrid/DispatchController.hpp
@@ -1,1 +1,29 @@
+#pragma once
+#include "models/pv/PvPanel.hpp"
+#include "models/storage/Battery.hpp"
+#include "models/converters/DcDcConverter.hpp"
+#include "controllers/mppt/MpptAlgorithm.hpp"
+#include <memory>
 
+namespace Microgrid::Controllers {
+    class DispatchController {
+    public:
+        DispatchController(
+            std::shared_ptr<Models::PvPanel> pv,
+            std::shared_ptr<Models::Battery> bat,
+            std::shared_ptr<Models::DcDcConverter> conv,
+            std::shared_ptr<MPPT::MpptAlgorithm> mppt);
+
+        // Runs a single timestep control loop. Returns the grid interaction power (excess/deficit)
+        double step(double time, double irradiance, double temp, double loadPower);
+
+    private:
+        std::shared_ptr<Models::PvPanel> pvSystem;
+        std::shared_ptr<Models::Battery> battery;
+        std::shared_ptr<Models::DcDcConverter> pvConverter;
+        std::shared_ptr<MPPT::MpptAlgorithm> mpptController;
+
+        double pvVoltageBus = 0.0; // Input side of the converter
+        double prevDuty = 0.5;
+    };
+}
