3017532b46733c95062d3f31b8a0daa708b6e9b5
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-06-26 17:28:27 -0700

    Add PvPanel class with specifications and methods

--- include/models/pv/PvPanel.hpp
+++ include/models/pv/PvPanel.hpp
@@ -1,1 +1,33 @@
+#pragma once
 
+namespace Microgrid::Models {
+    class PvPanel {
+    public:
+        struct Specs {
+            double Voc_ref;     // Open circuit voltage at STC
+            double Isc_ref;     // Short circuit current at STC
+            double Pmax_ref;    // Max power
+            double tempCoeff_V; // V/C
+            double tempCoeff_I; // A/C
+            double idealityFact;// Diode ideality
+        };
+
+        explicit PvPanel(Specs specifications);
+        
+        void updateEnvironment(double irradiance, double temperature);
+        
+        // Returns the current [A] given the operating voltage [V]
+        [[nodiscard]] double getCurrent(double operatingVoltage) const;
+        [[nodiscard]] double getPower(double operatingVoltage) const;
+
+    private:
+        Specs specs;
+        double currentIrradiance;
+        double currentTemp;
+        
+        // Dynamic variables adjusted for temp and irradiance
+        double Voc_adj;
+        double Isc_adj;
+        double Vt; // Thermal voltage
+    };
+}
